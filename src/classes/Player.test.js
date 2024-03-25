af218b4adb378c7f158dccb0af51473f4fcd96fa
Kellian <kellian.mirey.etu@univ-lille.fr>
2024-03-25 11:48:08 +0100

    Test : Player moving towards road borders

--- src/classes/Player.test.js
+++ src/classes/Player.test.js
@@ -1,6 +1,7 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import Player from './Player.js';
+import Road from './Road.js';
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
@@ -27,21 +28,39 @@ describe('Player', () => {
 	it('should move to the left', () => {
 		const player = new Player(100, 0, 64, 64);
 		player.velocity.x = -1;
-		player.update();
+		player.move();
 		assert.equal(player.getX() < 100, true);
 	});
 
 	it('should move upward', () => {
 		const player = new Player(0, 100, 64, 64);
 		player.velocity.y = -1;
-		player.update();
+		player.move();
 		assert.equal(player.getY() < 100, true);
 	});
 
 	it('should move downward', () => {
 		const player = new Player(0, 0, 64, 64);
 		player.velocity.y = 1;
-		player.update();
+		player.move();
 		assert.equal(player.getY() > 0, true);
+	});
+
+	it('should not move over road topborder', () => {
+		const road = new Road(500, 700);
+		const player = new Player(0, 600, 64, 64);
+		player.velocity.y = -1;
+		setInterval(player.move, 1000 / 60);
+		console.log(player.getY());
+		assert.equal(player.getY() <= road.getTopBorder(), false);
+	});
+
+	it('should not move over road bottomborder', () => {
+		const road = new Road(500, 700);
+		const player = new Player(0, 600, 64, 64);
+		player.velocity.y = 1;
+		setInterval(player.move, 1000 / 60);
+		console.log(player.getY());
+		assert.equal(player.getY() >= road.getBottomBorder(), false);
 	});
 });
