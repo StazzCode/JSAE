9d20d4341b497710086f3fffd3a5519e1c6d42c0
Kellian <kellian.mirey.etu@univ-lille.fr>
2024-04-03 10:07:19 +0200

    Merge branch Kellian

--- client/src/mainMenu.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('#play').addEventListener('hover', event => {
-	event.target.innerHtml = `<img src="/client/public/img/Buttons/doom-bigupper-play-2.png" alt="Play"></img>`;
-});
