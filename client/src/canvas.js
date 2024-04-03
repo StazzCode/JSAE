2398c9c24693c35e29d528cfd55d7daa8b8eb847
Baptiste Collesson <baptiste.collesson.etu@univ-lille.fr>
2024-04-03 10:19:36 +0200

    Ajout : Canvas

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -1,28 +1,19 @@
-// when socket io connected get id sand set userSocketID to it
-
-import { io } from 'socket.io-client';
-
-const socket = io();
-socket.on('connect', () => {
-	console.log(`connecté au serveur avec l'id ${socket.id}`);
-});
-
 const canvas = document.querySelector('.gameCanvas');
 const context = canvas.getContext('2d');
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keypress', event => {
     switch (event.key) {
         case "z":
-			socket.send('up');
+            console.log("haut");
             break;
         case "q":
-			socket.send('left');
+            
             break;
         case "s":
-			socket.send('down');
+            
             break;
         case "d":
-			socket.send('right');
+            
             break;
     }
 })
--- server/index.js
+++ server/index.js
@@ -51,3 +51,4 @@ const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
 });
+
