39a864584929142c71f23e52973a6f1f41688459
Linventif <contact@linv.dev>
2024-04-05 15:53:44 +0200

    add: config

---
+++ server/config/index.js
@@ -0,0 +1,10 @@
+exports.DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+exports.DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+exports.DISCORD_PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
+exports.DISCORD_CLIENT_SECRET = exports.APP_PORT = process.env.PORT || 3000;
+
+exports.POSTGRES_HOST = process.env.POSTGRES_HOST;
+exports.POSTGRES_PORT = process.env.POSTGRES_PORT;
+exports.POSTGRES_DB = process.env.POSTGRES_DB;
+exports.POSTGRES_USER = process.env.POSTGRES_USER;
+exports.POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
